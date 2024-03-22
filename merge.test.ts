51dd11d28bd36ae8853eb355590dddc29c2c0bd4
Kiratikorn <69286871+Kiratikorn@users.noreply.github.com>
2024-03-22 23:59:37 +0700

    merge.test.ts

--- merge.test.ts
+++ merge.test.ts
@@ -1,13 +1,4 @@
 import { merge } from './merge';
-// describe('merge function', () => {
-//     it('should merge three sorted arrays into one sorted array', () => {
-        // const collection1 = [1, 3, 5];
-        // const collection2 = [6, 4, 2];
-        // const collection3 = [7, 8, 9];
-        // const result = merge(collection1, collection2, collection3);
-        // expect(result).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-//     });
-// });
 
 describe('merge function', () => {
     it('should merge three sorted arrays', () => {
